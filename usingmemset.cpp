38ed4bbae93d26cc6a18df9f79b51317db226fa3
Ayushman Rayaguru <47988562+ayushman-rayaguru@users.noreply.github.com>
2021-03-26 14:06:10 +0530

    Add files via upload

---
+++ usingmemset.cpp
@@ -0,0 +1,14 @@
+# include<bits/stdc++.h>
+#include<cstring>
+using namespace std;
+#define FOR(n) for(int i = 0;i<n;i++) 
+int main()
+{
+    int arr[7];
+    memset(arr,-1,sizeof(arr));
+    FOR(7)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
